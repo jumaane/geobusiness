17904bfd2589183d7dd2892776669885d9e403e5
jumaane <jumesyn708@yahoo.com>
2016-11-13 19:10:13 +0900

    Add files via upload

---
+++ GeoRest.js
@@ -0,0 +1,75 @@
+var restaurantCollection =  "type": "FeatureCollection",
+    "features": [
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.655314,139.700995]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.63140, 139.7039]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6314,139.70395]},
+			"properties": {
+-				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		}
+	]
